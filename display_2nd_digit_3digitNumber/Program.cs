5bd4fb3886c4bc76722756a73d9bb36cb9db8987
Julia Machina <machinajg@gmail.com>
2023-02-03 16:40:35 +0300

    Display 2nd digit out of 3-digit number project added

---
+++ display_2nd_digit_3digitNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = numberA / 10;
+int numberC = numberB % 10;
+Console.WriteLine($"{numberC} - это вторая цифра числа {numberA}");
